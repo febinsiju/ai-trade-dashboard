efc900ce18e6e8b39ac9809ed420dffc91a2df1d
febinsiju <febinsijuarackan@gmail.com>
2026-03-02 20:34:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -652,8 +652,9 @@ st.markdown("---")
 # =============================
 ganga_img = get_base64_image("ganga_image.jpg").replace("\n", "")
 
-html_code = f"""
+st.markdown(f"""
 <div class="team-card fade-in">
+
     <div class="profile-container">
         <img src="data:image/jpeg;base64,{ganga_img}" class="profile-image">
         <div class="profile-overlay">
@@ -665,10 +666,9 @@ html_code = f"""
 
     <h3>Ganga AR</h3>
     <p class="role-title">Co-Founder & Research Strategist</p>
-</div>
-"""
 
-st.markdown(html_code, unsafe_allow_html=True)
+</div>
+""", unsafe_allow_html=True)
 
 st.markdown("""
 Ganga AR strengthens QuantNova’s research discipline through structured validation frameworks, reproducible experimentation processes, and rigorous analytical documentation. Her focus ensures the platform maintains academic integrity while evolving toward scalable AI intelligence infrastructure.
